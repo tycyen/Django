c552178ab41928279177096f196e0cf2261ce8e6
Anson Tsai <ansontsai@mit.com.sg>
2019-04-01 13:37:27 +0800

    issue form can transfer to target, target page so far something wrong.

--- myproject/polls/urls.py
+++ myproject/polls/urls.py
@@ -1,21 +1,13 @@
 from django.urls import path
-
+from django.views.generic import TemplateView
 from . import views
 
 app_name = 'polls'  #加了這一行,原本模板的{% url 'detail'就要改{% url 'polls:detail'
 
-'''urlpatterns = [
-    # ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
-]'''
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('issuefeedback', TemplateView.as_view(template_name='polls/issuefeedback.htm'), name = 'issuefeedback'),
+    path('issuefeedbackreply', TemplateView.as_view(template_name='polls/issuefeedbackreply.htm'), name = 'issuefeedbackreply'),
     path('<int:pk>/', views.FeatureChooseView.as_view(), name='feature'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
